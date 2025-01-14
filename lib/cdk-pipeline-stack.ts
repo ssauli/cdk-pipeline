815bcba7dfb901f5eac29ecff9de7ca008c41b63
sauli <sauli.tarvainen@skillwell.fi>
2025-01-14 16:12:45 +0200

    style: added prettier and formatted all files with prettier rules

--- lib/cdk-pipeline-stack.ts
+++ lib/cdk-pipeline-stack.ts
@@ -1,16 +1,18 @@
-import {pipelines, SecretValue, Stack, StackProps} from 'aws-cdk-lib';
-import {Construct} from 'constructs';
+import { pipelines, SecretValue, Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 
 export class CdkPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
-      synth : new pipelines.ShellStep('Synth', {
-        input : pipelines.CodePipelineSource.gitHub(
-            'ssauli/cdk-pipeline', 'main',
-            {authentication : SecretValue.secretsManager('github-token')}),
-        commands : [ 'npm ci', 'npm run build', 'npx cdk synth' ],
+      synth: new pipelines.ShellStep('Synth', {
+        input: pipelines.CodePipelineSource.gitHub(
+          'ssauli/cdk-pipeline',
+          'main',
+          { authentication: SecretValue.secretsManager('github-token') },
+        ),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
       }),
     });
   }
